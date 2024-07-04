02fd1e63dc15f87bdbead699dd94e55c48cad872
Type-32 <wilsonsu2008@hotmail.com>
2024-07-04 18:49:41 +0800

    Adding Plugins
    Signed-off-by: Type-32 <wilsonsu2008@hotmail.com>

--- lib/BasicBot.ts
+++ lib/BasicBot.ts
@@ -1,5 +1,22 @@
-class BasicBot{
-    public BasicBot(){
+import mineflayer = require('mineflayer');
 
+class BasicBot{
+    private bot: any = null;
+    public BasicBot(){
+        const hostUrl = process.env.HOST_URL,
+            portNumber = parseInt(process.env.PORT),
+            username = process.env.USERNAME,
+            password = process.env.PASSWORD;
+
+        this.bot = mineflayer.createBot({
+            host: hostUrl || 'localhost', // Change this to the ip you want.
+            port: portNumber as number || 25565, // Change this to the port you want.
+            client: undefined,
+            brand: 'vanilla',
+            auth: "microsoft",
+            username: username,
+            password: password,
+            version: "1.20.4",
+        })
     }
 }
--- test.ts
+++ test.ts
@@ -1,20 +1,8 @@
 import mineflayer = require('mineflayer');
 import * as dotenv from 'dotenv';
+// @ts-ignore
+import BasicBot from 'lib/BasicBot'
 dotenv.config();
 
-const hostUrl = process.env.HOST_URL,
-    portNumber = parseInt(process.env.PORT),
-    username = process.env.USERNAME,
-    password = process.env.PASSWORD;
-
-const bot = mineflayer.createBot({
-    host: hostUrl || 'localhost', // Change this to the ip you want.
-    port: portNumber as number || 25565, // Change this to the port you want.
-    client: undefined,
-    brand: 'vanilla',
-    auth: "microsoft",
-    username: username,
-    password: password,
-    version: "1.20.4",
-});
+const bot = new BasicBot();
 
